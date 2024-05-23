68b7813666bf73fa2e074fc61cf7a7b57a422e38
rachitpoudel101 <rachit300poudel@gmail.com>
2024-05-23 17:28:01 +0545

    Learn about the model and urls use in the   Blog

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from . models import Blog
 
-# Create your views here.
+def all_blogs(request):
+    blogs = Blog.objects.all()
+    
+    return render(request, 'blogs/all_blogs.html', {'blogs':blogs})
