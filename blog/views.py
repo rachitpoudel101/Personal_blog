e9037d75f010fca54ffea543c35007a2caa859ab
rachitpoudel101 <rachit300poudel@gmail.com>
2024-05-24 21:15:08 +0545

    add the blog in the asseding aordr

--- blog/views.py
+++ blog/views.py
@@ -2,6 +2,6 @@ from django.shortcuts import render
 from . models import Blog
 
 def all_blogs(request):
-    blogs = Blog.objects.all()
+    blogs = Blog.objects.order_by('-date')[:5]
     
     return render(request, 'blogs/all_blogs.html', {'blogs':blogs})
